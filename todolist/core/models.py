99de2f4006d0b85dd569c79b4b7e40cdc709798a
andrew <andrewmar78@yandex.ru>
2022-09-28 21:47:54 +0500

    model user added

---
+++ todolist/core/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+    ADMIN = "admin"
+    USER = "user"
+    ROLES = ((ADMIN, "Администратор"), (USER, "Пользователь"))
+
+    phone = models.CharField(max_length=20, null=True)
+    email = models.EmailField(unique=True, max_length=50)
+    role = models.CharField(max_length=5, null=True, choices=ROLES, default=USER)
+    image = models.ImageField(upload_to="user_images/", null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "phone", "role"]
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["email"]
+
+    def __str__(self):
+        return self.email
