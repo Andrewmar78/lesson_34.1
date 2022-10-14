488d226a3fffef150cbdea29ce6fc2795cf7e488
andrew <andrewmar78@yandex.ru>
2022-10-14 21:39:17 +0500

    admin added

--- todolist/core/admin.py
+++ todolist/core/admin.py
@@ -3,4 +3,21 @@ from django.contrib.auth.admin import UserAdmin
 
 from core.models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class AnyUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ('username', 'password')}),
+        (
+            'Personal info',
+            {'fields': ('first_name', 'last_name', 'email')}
+        ),
+        (
+            'Permissions',
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        (
+            'Important dates',
+            {'fields': ('last_login', 'date_joined')}
+        ),
+    )
