79cb3da93f7d9dfe501c5442e5deacc673854a06
andrew <andrewmar78@yandex.ru>
2022-11-20 22:00:21 +0500

    boards done

--- todolist/goals/urls.py
+++ todolist/goals/urls.py
@@ -4,13 +4,19 @@ from goals import views
 
 
 urlpatterns = [
-    path("goal_category/create", views.GoalCategoryCreateView.as_view(), name='create-category'),
-    path("goal_category/list", views.GoalCategoryListView.as_view(), name='cat-list'),
-    path("goal_category/<pk>", views.GoalCategoryView.as_view(), name='cat-detail'),
-    path("goal/create", views.GoalCreateView.as_view(), name='create-goal'),
+    path("board/create", views.BoardCreateView.as_view(), name='board-create'),
+    path("board/list", views.BoardListView.as_view(), name='board-list'),
+    path("board/<pk>", views.BoardView.as_view(), name='board-retrieve-update-destroy'),
+
+    path("goal_category/create", views.GoalCategoryCreateView.as_view(), name='category-create'),
+    path("goal_category/list", views.GoalCategoryListView.as_view(), name='category-list'),
+    path("goal_category/<pk>", views.GoalCategoryView.as_view(), name='category-retrieve-update-destroy'),
+
+    path("goal/create", views.GoalCreateView.as_view(), name='goal-create'),
     path("goal/list", views.GoalListView.as_view(), name='goal-list'),
-    path("goal/<pk>", views.GoalView.as_view(), name='goal-detail'),
-    path("goal_comment/create", views.GoalCommentCreateView.as_view(), name='create-comment'),
+    path("goal/<pk>", views.GoalView.as_view(), name='goal-retrieve-update-destroy'),
+
+    path("goal_comment/create", views.GoalCommentCreateView.as_view(), name='comment-create'),
     path("goal_comment/list", views.GoalCommentListView.as_view(), name='comment-list'),
-    path("goal_comment/<pk>", views.GoalCommentView.as_view(), name='comment-detail'),
+    path("goal_comment/<pk>", views.GoalCommentView.as_view(), name='comment-retrieve-update-destroy'),
 ]
